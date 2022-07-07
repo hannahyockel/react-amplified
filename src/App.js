c9efeacfc910aa5a5882225b18c51839611393a6
hannahyockel <88843058+hannahyockel@users.noreply.github.com>
2022-07-06 19:22:09 -0500

    add some grid layout

--- src/App.js
+++ src/App.js
@@ -1,119 +1,144 @@
 /* src/App.js */
-import React, { useEffect, useState } from 'react'
-import { Amplify, API, graphqlOperation } from 'aws-amplify'
-import { createTodo } from './graphql/mutations'
-import { listTodos } from './graphql/queries'
+import React, { useEffect, useState } from "react";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
 import {
   defaultDarkModeOverride,
   ThemeProvider,
   Card,
   Text,
   useTheme,
+  Grid,
+  View,
   withAuthenticator,
   ToggleButton,
   ToggleButtonGroup,
   Button,
   Heading,
-  TextField
-} from '@aws-amplify/ui-react';
-import './App.css'
-import '@aws-amplify/ui-react/styles.css';
+  TextField,
+} from "@aws-amplify/ui-react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '' }
+const initialState = { name: "", description: "" };
 
 function App({ signOut, user }) {
-  const [formState, setFormState] = useState(initialState)
-  const [todos, setTodos] = useState([])
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState([]);
 
   // When the component loads, the useEffect hook is called and it invokes the fetchTodos function
   useEffect(() => {
-    fetchTodos()
-  }, [])
+    fetchTodos();
+  }, []);
 
   function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
+    setFormState({ ...formState, [key]: value });
   }
 
   // Uses the Amplify API category to call the AppSync GraphQL API with the listTodos query
   // Once the data is returned, the items array is passed in to the setTodos function to update the local state
   async function fetchTodos() {
     try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
   }
 
   // Uses the Amplify API category to call the AppSync GraphQL API with the createTodo mutation
   // A difference between the listTodos query and the createTodo mutation is that createTodo accepts an argument containing the variables needed for the mutation
   async function addTodo() {
     try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+      if (!formState.name || !formState.description) return;
+      const todo = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
     } catch (err) {
-      console.log('error creating todo:', err)
+      console.log("error creating todo:", err);
     }
   }
-  const [colorMode, setColorMode] = React.useState('system');
+  const [colorMode, setColorMode] = React.useState("system");
   const theme = {
-    name: 'my-theme',
+    name: "my-theme",
     overrides: [defaultDarkModeOverride],
   };
 
   return (
     <>
-    <ThemeProvider theme={theme} colorMode={colorMode}>
-      
-    <Card className="app">
-    
-      
-    <Heading level={5}>Hello user {user.username}</Heading>
-      <Button onClick={signOut}>Sign out</Button>
-      <Heading level={2}>Todos</Heading>
-      <TextField
-        onChange={event => setInput('name', event.target.value)}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <TextField
-        onChange={event => setInput('description', event.target.value)}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <Button onClick={addTodo}>Create Todo</Button>
-      
-      {
-        todos.map((todo, index) => (
-          <Card variation="elevated" key={todo.id ? todo.id : index}>
-            <p>{todo.name}</p>
-            <p>{todo.description}</p>
-            {/* <button style={styles.button} onClick={removeTodo}>delete</button> */}
+      <ThemeProvider theme={theme} colorMode={colorMode}>
+        <Card>
+          <Grid
+            columnGap="0.5rem"
+            rowGap="0.5rem"
+            templateColumns="1fr 1fr 1fr"
+            templateRows="1fr 3fr 1fr"
+          >
+            <Card columnStart="1" columnEnd="-1">
+              <Heading level={5}>Hello user {user.username}</Heading>
+              <Button onClick={signOut}>Sign out</Button>
+            </Card>
 
-          </Card>
-        ))
-      }
-      <Card>
-        <ToggleButtonGroup
-          value={colorMode}
-          isExclusive
-          onChange={(value) => setColorMode(value)}
-        >
-          <ToggleButton value="light">Light</ToggleButton>
-          <ToggleButton value="dark">Dark</ToggleButton>
-          <ToggleButton value="system">System</ToggleButton>
-        </ToggleButtonGroup>
-        <Text>Current color mode: {colorMode}</Text>
-      </Card>
-    </Card>
+            <Card columnStart="1" columnEnd="2">
+              <ToggleButtonGroup
+                value={colorMode}
+                isExclusive
+                onChange={(value) => setColorMode(value)}
+              >
+                <ToggleButton value="light">Light</ToggleButton>
+                <ToggleButton value="dark">Dark</ToggleButton>
+                <ToggleButton value="system">System</ToggleButton>
+              </ToggleButtonGroup>
+              <Text>Current color mode: {colorMode}</Text>
+            </Card>
 
-    </ThemeProvider>
-      </>
+            <Card columnStart="2" columnEnd="-1">
+              <Heading level={2}>Todos</Heading>
+              <TextField
+                onChange={(event) => setInput("name", event.target.value)}
+                value={formState.name}
+                placeholder="Name"
+              />
+              <TextField
+                onChange={(event) =>
+                  setInput("description", event.target.value)
+                }
+                value={formState.description}
+                placeholder="Description"
+              />
+              <Button onClick={addTodo}>Create Todo</Button>
+
+              {todos.map((todo, index) => (
+                <Card key={todo.id ? todo.id : index}>
+                  <Text variation="info" as="p" width="33vw">
+                    {todo.name}
+                  </Text>
+                  <Text
+                    isTruncated={true}
+                    variation="primary"
+                    as="p"
+                    width="33vw"
+                  >
+                    {todo.description}
+                  </Text>
+                  {/* <button style={styles.button} onClick={removeTodo}>delete</button> */}
+                </Card>
+              ))}
+            </Card>
+
+            <Card columnStart="2" columnEnd="-1">
+              Footer
+            </Card>
+          </Grid>
+        </Card>
+      </ThemeProvider>
+    </>
   );
 }
 
